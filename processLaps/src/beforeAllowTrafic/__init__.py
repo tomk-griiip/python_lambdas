5df7959e85853a2a5fdd4135d4197aef2478fc09
tomk-griiip <tom.k@griiip.com>
2020-06-24 11:23:05 +0300

    remove beforeAllowTrafic function

--- processLaps/src/beforeAllowTrafic/__init__.py
+++
@@ -1,5 +0,0 @@
-import os
-import logging
-logging.basicConfig(format='%(asctime)s %(levelname)8s: %(message)s', datefmt='%m-%d %H:%M:%S')
-logger = logging.getLogger(__name__)
-logger.setLevel(int(os.environ['logLevel']))
