c0a1f838fa975c7829fc1736f1b012cb77bcfde7
tomk-griiip <tom.k@griiip.com>
2020-07-28 14:33:51 +0300

    add cloud watch

--- processLaps/src/producer/__init__.py
+++ processLaps/src/producer/__init__.py
@@ -2,10 +2,12 @@ import os
 import sys
 import inspect
 import logging
+import watchtower
 current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, f"{current_dir}")
 sys.path.insert(1, f"{parent_dir}")
 # add log level
 logger = logging.getLogger(__name__)
+logger.addHandler(watchtower.CloudWatchLogHandler())
 logger.setLevel(int(os.environ['logLevel']))
